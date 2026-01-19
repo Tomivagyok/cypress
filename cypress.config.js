4df1380a5e2f4cbfa47898abac0fb179b86996fa
Tamás Gálfi <gofifi1995@gmail.com>
2026-01-19 02:03:34 +0100

    Rename test file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://www.globalsqa.com',
-        supportFile: false,
-        specPattern: '/e2e/**/*.cy.js'
+        supportFile: false
     },
 })
