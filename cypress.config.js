511f15b93a362ac7478e3d7fb6151f6cccc65301
Tamás Gálfi <gofifi1995@gmail.com>
2026-01-19 02:00:33 +0100

    Update config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://www.globalsqa.com',
-        supportFile: false
+        supportFile: false,
+        specPattern: 'cypress/e2e/**/*.cy.js'
     },
 })
